9c4932c38933f7732d3c7320b483c1013544c83b
Rohan Prkash N <119556917+rprkash10@users.noreply.github.com>
2023-05-11 09:36:41 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,29 @@ const logoName = document.getElementById("logo-name");
 const body = document.querySelector("body");
 const navbar = document.querySelector("nav");
 
+const toggleSwitch = document.querySelector('.toggle__input');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
 window.addEventListener("DOMContentLoaded", function () {
   // Get the form elements defined in your form HTML above
   var form = document.getElementById("contact-form");
